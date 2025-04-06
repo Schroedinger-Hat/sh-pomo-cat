031ec9408b8a1514e1a3141275f33ea92907e99f
Patrick Raedler <patrick.raedler@schroedinger-hat.org>
2025-04-06 17:37:07 +0200

    feat(app.vue): add initial functionality for pomodoro

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,42 @@
+<script setup lang="ts">
+const pomodoro = shallowRef(0.05 * 60)
+const completions = shallowRef(0)
+
+const { pause, resume, isActive } = useIntervalFn(() => {
+  pomodoro.value = pomodoro.value - 1
+  if (pomodoro.value === 0) {
+    pause()
+    completions.value = completions.value + 1
+    pomodoro.value = 25 * 60
+  }
+}, 1000, { immediate: false })
+
+const timeLeft = computed(() => {
+  const minutes = Math.floor(pomodoro.value / 60)
+  const seconds = pomodoro.value % 60
+  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+})
+
+function reset() {
+  pomodoro.value = 25 * 60
+  if (isActive.value) {
+    pause()
+  }
+}
+</script>
+
 <template>
-  <SHWelcome />
+  <CtaColorMode />
+  {{ completions }}
+  <SCButton :disabled="isActive" @click="resume">
+    resume
+  </SCButton>
+  <SCButton @click="pause">
+    pause
+  </SCButton>
+  <SCButton @click="reset">
+    reset
+  </SCButton>
+  <p>{{ isActive }}</p>
+  <div>{{ timeLeft }}</div>
 </template>
