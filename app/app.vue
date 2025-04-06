b89b50610500aead003c2373a94131a6867b25ce
Patrick Raedler <patrick.raedler@schroedinger-hat.org>
2025-04-06 17:47:19 +0200

    refactor(app.vue): utilize composable

--- app/app.vue
+++ app/app.vue
@@ -1,34 +1,18 @@
 <script setup lang="ts">
-const pomodoro = shallowRef(0.05 * 60)
-const completions = shallowRef(0)
-
-const { pause, resume, isActive } = useIntervalFn(() => {
-  pomodoro.value = pomodoro.value - 1
-  if (pomodoro.value === 0) {
-    pause()
-    completions.value = completions.value + 1
-    pomodoro.value = 25 * 60
-  }
-}, 1000, { immediate: false })
-
-const timeLeft = computed(() => {
-  const minutes = Math.floor(pomodoro.value / 60)
-  const seconds = pomodoro.value % 60
-  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
-})
-
-function reset() {
-  pomodoro.value = 25 * 60
-  if (isActive.value) {
-    pause()
-  }
-}
+const {
+  completions,
+  isActive,
+  pause,
+  reset,
+  start,
+  timeLeft,
+} = usePomodoro()
 </script>
 
 <template>
   <CtaColorMode />
   {{ completions }}
-  <SCButton :disabled="isActive" @click="resume">
+  <SCButton :disabled="isActive" @click="start">
     resume
   </SCButton>
   <SCButton @click="pause">
