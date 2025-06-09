a5c2f18055792451af00bc712b17829416713a92
Patrick Raedler <patrick.raedler@schroedinger-hat.org>
2025-06-09 16:27:27 +0200

    fix(components/PomodoroButton): remove recursivness

--- app/components/pomodoro/PomodoroButton.vue
+++ app/components/pomodoro/PomodoroButton.vue
@@ -5,12 +5,12 @@ defineProps<{
 </script>
 
 <template>
-  <PomodoroButton class="md:w-auto md:min-w-20 mt-2 min-w-20 leading-none">
+  <Button class="md:w-auto md:min-w-20 mt-2 min-w-20 leading-none">
     <BaseIcon
       v-if="icon"
       class="size-5"
       :icon
     />
     <slot />
-  </PomodoroButton>
+  </Button>
 </template>
