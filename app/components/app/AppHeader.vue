9a88b9360f6167f91281b50fb73d9c5aab474518
Patrick Raedler <patrick.raedler@schroedinger-hat.org>
2025-06-09 16:34:15 +0200

    feat(components/app/Header): create header component for whole app

---
+++ app/components/app/AppHeader.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import SHLogo from '~/assets/svg/sh-logo.svg'
+</script>
+
+<template>
+  <header
+    class="h-16 border-b flex justify-center items-center p-6"
+  >
+    <div class="mr-auto flex justify-center items-center gap-x-2">
+      <SHLogo class="size-8" />
+      <p class="font-medium text-sm md:text-lg leading-none">
+        Schrödinger PomoCat
+      </p>
+    </div>
+    <nav class="flex gap-x-2">
+      <CtaIcon
+        as-child
+        icon="logo-github"
+        is-anchor
+        target="_blank"
+        to="https://github.com/schroedinger-hat"
+      />
+      <CtaColorMode />
+    </nav>
+  </header>
+</template>
