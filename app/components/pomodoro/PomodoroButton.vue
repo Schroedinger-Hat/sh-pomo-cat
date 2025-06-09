abe750e27dcbb1825aee8d0ffdd68b91545a9a47
Patrick Raedler <patrick.raedler@schroedinger-hat.org>
2025-06-09 16:11:00 +0200

    refactor(components/PomodoroButton): add icon

--- app/components/pomodoro/PomodoroButton.vue
+++ app/components/pomodoro/PomodoroButton.vue
@@ -5,12 +5,12 @@ defineProps<{
 </script>
 
 <template>
-  <Button class="md:w-auto md:min-w-20 mt-2 min-w-20 leading-none">
+  <PomodoroButton class="md:w-auto md:min-w-20 mt-2 min-w-20 leading-none">
     <BaseIcon
       v-if="icon"
       class="size-5"
       :icon
     />
     <slot />
-  </Button>
+  </PomodoroButton>
 </template>
