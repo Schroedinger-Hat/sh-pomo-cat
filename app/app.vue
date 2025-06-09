710177ea4beca24918209c92e9e02277ea04eb41
Patrick Raedler <patrick.raedler@schroedinger-hat.org>
2025-06-09 16:05:13 +0200

    refactor(app): simplify app.vue component, implement new components

--- app/app.vue
+++ app/app.vue
@@ -11,20 +11,8 @@ const { height: windowHeight } = useWindowSize()
 const contentHeight = computed(() => windowHeight.value - headerHeight.value)
 
 const {
-  isBreak,
-  completions,
-  currentCycle,
-  isActive,
-  pause,
-  reset,
-  start,
   timeLeft,
-  skip,
 } = usePomodoro()
-
-function handleTimerInteraction() {
-  isActive.value ? pause() : start()
-}
 
 useHead({
   title: () => `${timeLeft.value} | Schrödinger PomoCat`,
@@ -71,78 +59,8 @@ useSeoMeta({
       <main class="mx-auto bg-transparent max-w-4xl w-full container p-6 z-20 relative">
         <Card class="w-full">
           <CardContent class="grid grid-cols-12 md:gap-10 gap-4 divide-y md:divide-y-0 md:divide-x">
-            <div class="col-span-12 md:col-span-6 w-full pb-4 md:pb-0 space-y-3">
-              <div class="w-full">
-                <Badge
-                  variant="outline"
-                  class="mx-auto block"
-                >
-                  {{ isBreak ? 'Break' : 'Pomodoro' }}
-                </Badge>
-              </div>
-              <p class="text-7xl tabular-nums text-center w-full">
-                {{ timeLeft }}
-              </p>
-              <div class="flex justify-center items-center gap-x-2 w-full">
-                <Button
-                  class="md:w-auto md:min-w-20 mt-2 min-w-20 leading-none transition-colors bg-green-200 hover:bg-green-300"
-                  :class="{ 'bg-red-200 hover:bg-red-300': isActive }"
-                  @click="handleTimerInteraction"
-                >
-                  <BaseIcon
-                    :icon="isActive ? 'pause' : 'play'"
-                    class="size-5"
-                  />
-                  <span>{{ isActive ? 'Pause' : 'Start' }}</span>
-                </Button>
-                <Button
-                  v-if="isActive"
-                  class="md:w-auto md:min-w-20 mt-2 min-w-20 leading-none"
-                  @click="reset"
-                >
-                  <BaseIcon
-                    icon="restart"
-                    class="size-5"
-                  />
-                  Reset
-                </Button>
-                <Button
-                  class="md:w-auto md:min-w-20 mt-2 min-w-20 leading-none"
-                  @click="skip"
-                >
-                  <BaseIcon
-                    icon="skip-forward"
-                    class="size-5"
-                  />
-                  Skip
-                </Button>
-              </div>
-            </div>
-            <dl class="col-span-12 md:col-span-6 content-center space-y-2 text-sm/6">
-              <div class="flex flex-col md:flex-row justify-between gap-x-6 gap-y-2 md:gap-y-0 items-center">
-                <dt class="flex-auto font-semibold mx-auto">
-                  Current cycle
-                </dt>
-                <dd class="transition-transform shrink-0 flex justify-center items-center space-x-2">
-                  <img
-                    v-for="n in 4"
-                    :key="n"
-                    class="size-6"
-                    src="~/assets/images/schroddy.png"
-                    :class="{ grayscale: n > currentCycle }"
-                    alt="Logo of Schrödinger Hat"
-                  >
-                </dd>
-              </div>
-              <div class="flex flex-col md:flex-row justify-between gap-x-6 gap-y-2 md:gap-y-0 items-center">
-                <dt class="flex-auto font-semibold mx-auto">
-                  Completed PomoCats
-                </dt>
-                <dd class="shrink-0 sm:flex sm:flex-col md:items-end mx-auto">
-                  {{ completions }}
-                </dd>
-              </div>
-            </dl>
+            <PomodoroCountdown class="col-span-12 md:col-span-6" />
+            <PomodoroStats class="col-span-12 md:col-span-6 content-center" />
           </CardContent>
         </Card>
       </main>
