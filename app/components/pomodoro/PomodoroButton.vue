2f731d48131dd28de14365736442418a9fa27ad1
Patrick Raedler <patrick.raedler@schroedinger-hat.org>
2025-06-09 16:03:24 +0200

    feat(components/pomodoro/Button): create button for the pomodoro countdown

---
+++ app/components/pomodoro/PomodoroButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  icon?: string
+}>()
+</script>
+
+<template>
+  <Button class="md:w-auto md:min-w-20 mt-2 min-w-20 leading-none">
+    <BaseIcon
+      v-if="icon"
+      class="size-5"
+      :icon
+    />
+    <slot />
+  </Button>
+</template>
